0469d3a17f3c931d8621e113156414008c3c8e13
zane grosse <zanekg123@gmail.com>
2025-11-30 01:29:32 -0600

    Google analytics & MS clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import "./globals.css";
+import Script from "next/script";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 import {Analytics} from "@vercel/analytics/next";
+import {GoogleAnalytics} from "@next/third-parties/google";
 
 export const metadata = {
   title: "Cornerstone",
@@ -19,6 +21,16 @@ export default function RootLayout({ children }) {
         </div>
         <Footer />
         <Analytics />
+        <GoogleAnalytics gaId={"G-4FX6994NZT"}/>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ue360a9kx0");
+          `}
+        </Script>
       </body>
     </html>
   );
