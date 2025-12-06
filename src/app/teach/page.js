10adf5eca21d14395fd70367731ac11d09354b5c
zane grosse <zanekg123@gmail.com>
2025-12-06 12:32:43 -0600

    Revert "Teach"
    This reverts commit 9797d92197ce56a1f92dbdb1ea594ebdda684973.

--- src/app/teach/page.js
+++ src/app/teach/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export const metadata = {
-  title: "Please, Teach we need this",
+  title: "Project Protocol - Cornerstone",
   description: "Formal Request for Cross-Class ISP Authorization",
 };
 
