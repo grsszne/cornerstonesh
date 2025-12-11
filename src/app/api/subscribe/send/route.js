2ee3622039ab60da3fe1ee6368a3a041a22ce19b
EdgarIjehor <eijehor@gmail.com>
2025-12-10 18:23:01 -0600

    route resend

---
+++ src/app/api/subscribe/send/route.js
@@ -0,0 +1,16 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  const response = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: ['delivered@resend.dev'],
+    subject: 'hello world',
+    html: '<strong>it works!</strong>',
+  });
+
+  return Response.json(response, {
+    status: response.error ? 500 : 200,
+  });
+}
